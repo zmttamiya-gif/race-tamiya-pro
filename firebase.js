2b3e6e4b686b7b43a47c2584d75b3f6fb29b351c
zmttamiya-gif <zmttamiya@gmail.com>
2026-01-12 17:23:32 +0700

    Update firebase.js
    Signed-off-by: zmttamiya-gif <zmttamiya@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,1 +1,7 @@
-// Firebase Config
+var firebaseConfig = {
+  apiKey: "PASTE_API_KEY",
+  authDomain: "tamiya4wd-thecustom.firebaseapp.com",
+  databaseURL: "https://tamiya4wd-thecustom-default-rtdb.firebaseio.com",
+  projectId: "tamiya4wd-thecustom"
+};
+firebase.initializeApp(firebaseConfig);
