ee857565ffbb11edd4a0339f4ba7c5e7f6e97f3c
zmttamiya-gif <zmttamiya@gmail.com>
2026-01-13 14:15:49 +0700

    Update firebase.js
    Signed-off-by: zmttamiya-gif <zmttamiya@gmail.com>

--- firebase.js
+++ firebase.js
@@ -1,7 +1,11 @@
-var firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyAZ2yV4RvtI9bVSdBG_wYRa3M0Yi0zrELE",
   authDomain: "tamiya4wd-thecustom.firebaseapp.com",
-  databaseURL: "https://tamiya4wd-thecustom-default-rtdb.firebaseio.com",
-  projectId: "tamiya4wd-thecustom"
+  databaseURL: "https://tamiya4wd-thecustom-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tamiya4wd-thecustom",
+  storageBucket: "tamiya4wd-thecustom.firebasestorage.app",
+  messagingSenderId: "961521863552",
+  appId: "1:961521863552:web:12263dd3055b2a2074cf65",
+  measurementId: "G-WM7KRTXW01"
 };
-firebase.initializeApp(firebaseConfig);
