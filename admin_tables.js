eed3761c462d664d7e75ef148919c90fd4415407
zmttamiya-gif <zmttamiya@gmail.com>
2026-01-13 17:21:50 +0700

    Update admin_tables.js
    Signed-off-by: zmttamiya-gif <zmttamiya@gmail.com>

--- admin_tables.js
+++ admin_tables.js
@@ -1,1 +1,12 @@
-// Multi Admin / Multi Table Logic
+const raceBody = document.getElementById("raceBody");
+
+for (let i = 1; i <= 5; i++) {
+  const tr = document.createElement("tr");
+  tr.innerHTML = `
+    <td>${i}</td>
+    <td>-</td>
+    <td>-</td>
+    <td>-</td>
+  `;
+  raceBody.appendChild(tr);
+}
